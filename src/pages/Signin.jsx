a3c2702f3da1e0cc08b95d0b6e19e1c34d593618
swilhoit <samwilhoit@gmail.com>
2025-07-03 08:42:25 -0700

    Add debug tools for OpenAI API key issue
    - Add debug-env.html page to troubleshoot environment variables
    - Update server to expose debug endpoints
    - Remove nixpacks config that was causing build errors

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -141,19 +141,10 @@ function Signin() {
               {/* Footer */}
               <div className="pt-5 mt-6 border-t border-gray-100 dark:border-gray-700/60">
                 <div className="text-sm">
-                  Don’t you have an account?{" "}
+                  Don't you have an account?{" "}
                   <Link className="font-medium text-violet-500 hover:text-violet-600 dark:hover:text-violet-400" to="/signup">
                     Sign Up
                   </Link>
-                </div>
-                {/* Warning */}
-                <div className="mt-5">
-                  <div className="bg-yellow-500/20 text-yellow-700 px-3 py-2 rounded">
-                    <svg className="inline w-3 h-3 shrink-0 fill-current mr-2" viewBox="0 0 12 12">
-                      <path d="M10.28 1.28L3.989 7.575 1.695 5.28A1 1 0 00.28 6.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 1.28z" />
-                    </svg>
-                    <span className="text-sm">To support you during the pandemic super pro features are free until March 31st.</span>
-                  </div>
                 </div>
               </div>
             </div>
